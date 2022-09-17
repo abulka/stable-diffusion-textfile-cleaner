e3c24671af7fadfc4742756e3982f8b4f3d87d21
Andy Bulka <abulka@gmail.com>
2022-09-17 13:45:38 +1000

    ui: expanded path textfield

--- ui.py
+++ ui.py
@@ -8,35 +8,9 @@ from main import list_files, create_two_sets, find_missing_files, add_txt_extens
 bad_txt_files = []
 chosen_path = DIR  # user can override later
 
+
 def main(page: Page):
     page.title = "stable diffustion textfile cleaner"
-
-    btnPick = ElevatedButton("Choose DIR...",
-        on_click=lambda _: file_picker.get_directory_path())
-    txt1 = TextField(label="DIR", value=DIR)
-    row = Row(spacing=0, controls=[
-              btnPick, 
-              txt1], 
-        alignment="start")
-    page.add(row)
-
-    def on_dialog_result(e: FilePickerResultEvent):
-        global chosen_path
-        print("Selected files:", e.files)
-        print("Selected file or directory:", e.path)
-        chosen_path = e.path
-        chosen_path = chosen_path.replace('\\', '')
-        if '/Volumes/Google Drive/' in chosen_path:
-            chosen_path = chosen_path.replace('/Volumes/Google Drive/', f"{os.path.expanduser('~')}/Google Drive/")
-        txt1.value = chosen_path
-        txt1.update()
-
-    def buttonDelete_clicked(e):
-        for file_path in bad_txt_files:
-            print('Would delete', file_path)
-            # os.remove(file_path)
-            if not os.path.exists(file_path):
-                print('  File does not exist', file_path)
 
     def button_clicked(e):
         global bad_txt_files
@@ -55,14 +29,47 @@ def main(page: Page):
             lv.controls.append(Text(f"{line}"))
         lv.update()
 
+    btnPick = ElevatedButton("Choose DIR...",
+                             on_click=lambda _: file_picker.get_directory_path())
+    txt1 = TextField(label="DIR", value=DIR, expand=True)
     btnScan = FilledTonalButton(
         "Scan", icon=icons.FIND_IN_PAGE, on_click=button_clicked)
+    row = Row(spacing=0, controls=[
+              btnPick,
+              txt1,
+              btnScan
+              ],
+              alignment="start")
+    page.add(row)
+
+    def on_dialog_result(e: FilePickerResultEvent):
+        global chosen_path
+        print("Selected files:", e.files)
+        print("Selected file or directory:", e.path)
+        chosen_path = e.path
+        chosen_path = chosen_path.replace('\\', '')
+        if '/Volumes/Google Drive/' in chosen_path:
+            chosen_path = chosen_path.replace(
+                '/Volumes/Google Drive/', f"{os.path.expanduser('~')}/Google Drive/")
+        txt1.value = chosen_path
+        txt1.update()
+
+    def buttonDelete_clicked(e):
+        for file_path in bad_txt_files:
+            if not os.path.exists(file_path):
+                print('  File does not exist', file_path)
+            else:
+                print('Would delete', file_path)
+                # os.remove(file_path)
+
+
     btnDeleteOrphans = ElevatedButton("Delete Orphans", icon=icons.DELETE, on_click=buttonDelete_clicked, style=ButtonStyle(
         bgcolor={"focused": colors.RED_200, "": colors.RED_900},
     ))
-    row = Row(spacing=0, controls=[
-              btnScan, btnDeleteOrphans], alignment="spaceEvenly")
-    page.add(row)
+    # row = Row(spacing=0, controls=[
+    #           btnScan, btnDeleteOrphans], alignment="spaceEvenly")
+    # page.add(row)
+    page.add(btnDeleteOrphans)
 
     lv = ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
 
@@ -71,7 +78,6 @@ def main(page: Page):
     file_picker = FilePicker(on_result=on_dialog_result)
     page.overlay.append(file_picker)
     page.update()
-
 
 
 flet.app(target=main)
