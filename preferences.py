4782d9c526d7bd1a9fa75935991c74e945d3204e
Andy Bulka <abulka@gmail.com>
2022-09-18 21:54:51 +1000

    feat: multiline text in preferences

--- preferences.py
+++ preferences.py
@@ -1,11 +1,13 @@
 import flet
-from flet import AlertDialog, ElevatedButton, Page, Text, TextButton, Column, Container, colors
+from flet import AlertDialog, ElevatedButton, Page, Text, TextButton, Column, Container, colors, TextField, icons, padding
 
 page = None
+
 
 def set_page(p):
     global page
     page = p
+
 
 def edit_preferences(e):
     print('edit_preferences')
@@ -13,24 +15,40 @@ def edit_preferences(e):
     dlg_modal.open = True
     page.update()
 
+
 def close_dlg(e):
     dlg_modal.open = False
     page.update()
 
+def get_strings_to_strip():
+    my_data = [
+        '_RealESRGAN_x4plus',
+        '_GFPGANv1.3_RealESRGAN_x4plus',
+    ]
+    # return my_data list as string with newlines
+    return '\n'.join(my_data)
+
+
 dlg_modal = AlertDialog(
     modal=True,
-    title=Text("Please confirm"),
-    # content=Text("Do you really want to delete all those files?"),
+    title=Text("Preferences"),
     content=Column(controls=[
-        Text("Do you really want to delete all those files?"),
-        Text("Do you really want to delete all those files?"),
-        Text("Do you really want to delete all those files?"),
-        Text("Do you really want to delete all those files?"),
+        # Text("Do you really want to delete all those files?"),
+
+        TextField(
+            label="strip these strings from png filenames",
+            multiline=True,
+            disabled=False,
+            max_lines=10,
+            value=get_strings_to_strip(),
+        ),
+
+        # Trick dialog to be wider
         Container(
-            bgcolor=colors.YELLOW,
-            height= 100,
-            width= 100,
-            ),
+            bgcolor=colors.TRANSPARENT,
+            height=1,
+            width=500,
+        ),
     ]),
     actions=[
         TextButton("Yes", on_click=close_dlg),
